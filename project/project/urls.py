eab9163afbc882a0555527434e92a3f4a46ddd6b
Alexandre de O. Zamberlan <alexandre.o.zamberlan@gmail.com>
2021-11-08 10:01:50 -0300

    app core and template

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,21 +1,36 @@
-"""project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('core.urls')),
+    # url(r'appadministracao/', include('core.urls')),
+    # url(r'appaexterno/', include('appexterno.urls')),
+    # url(r'criterio_avaliacao/', include('criterio_avaliacao.urls')),
+    # url(r'appaluno/', include('appaluno.urls')),
+    # url(r'appprofessor/', include('appprofessor.urls')),
+    # url(r'avaliacao/', include('avaliacao.urls')),
+    # url(r'curso/', include('curso.urls')),
+    # url(r'disciplina/', include('disciplina.urls')),
+    # url(r'documento/', include('documento.urls')),
+    # url(r'submissao/', include('submissao.urls')),
+    # url(r'turma/', include('turma.urls')),
+    # url(r'usuario/', include('usuario.urls')),
+   
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    # url(r'', include('landpage.urls')),
 ]
+
+#url para arquivos de media quando em desenvolvimento
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += static(settings.STATIC_URL, 
+    document_root = settings.STATIC_ROOT)    
+  
