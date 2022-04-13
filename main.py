254368c10051cff4dd332ebfde9634b397e25e92
Keliton Amaral <81647578+KelitonAmaral@users.noreply.github.com>
2022-04-13 09:57:17 -0300

    Added teste_3.py to minimize granularity

--- main.py
+++ main.py
@@ -14,7 +14,24 @@ my_video = Lift(r'project\upload\medias\snatch.mp4')
 my_trajectory = my_video.find_trajectory()
 
 # print("Trajetória original: ")
-# print(my_trajectory)
+# print(len(my_trajectory))
+x = 0
+i = 0
+tamanho = len(my_trajectory)
+while i < tamanho:
+    if i % 3 == 0:
+        x += 1
+        # print(i, " ", my_trajectory[i])
+        del my_trajectory[i]
+        tamanho = len(my_trajectory)
+    i += 1 
+
+# for i in range(20):
+    # del my_trajectory[0]
+
+# print("Contador: ", x)
+# print(len(my_trajectory))
+
 
 graph = Analysis()
 
